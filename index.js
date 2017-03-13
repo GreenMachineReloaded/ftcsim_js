b8895fa4c4875d7dc4fbefa87ee435d3e9471b15
Jeremy School <jschool504@gmail.com>
2017-03-13 17:15:17 -0400

    added text area that is run inside the main update loop

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ window.onload = function() {
 
     App.objects.tgt = new Target(ctx, 10, 350);
 
+	App.code = '';
+
     App.keysPressed = [];
 
     document.addEventListener("keydown", function(e) {
@@ -26,6 +28,12 @@ window.onload = function() {
     document.addEventListener("keyup", function(e) {
         App.keysPressed[e.keyCode] = false; // indicates that the key is no longer pressed
     });
+
+	var runButton = document.getElementsByClassName("run-button")[0];
+	runButton.addEventListener("click", function(e) {
+		var botCodeTextArea = document.querySelector(".code > textarea");
+		App.code = botCodeTextArea.value;
+	});
 
     window.setInterval(update, 5, 5);
     window.setInterval(draw, 21, 21);
@@ -42,123 +50,20 @@ function update(interval) {
 	var yDistToBeacon = by - ty;
     var currentAngle = Math.atan2((bx-tx), (by-ty))*(180/Math.PI);
 
-    if (App.currentState === "START") {
-        if (!App.stateTime) {
-            App.stateTime = Date.now();
-        } else if (Date.now() - App.stateTime > 1700) {
-            App.stateTime = null;
-            App.currentState = "STRAFE_LEFT_TO_BEACON";
-        }
-
-        App.objects.bot.setVelocityY(0.7);
-    }
-
-    if (App.currentState === "REV") {
-        if (!App.stateTime) {
-            App.stateTime = Date.now();
-        } else if (Date.now() - App.stateTime > 10000) {
-            App.stateTime = null;
-            App.currentState = "START";
-        }
-
-        App.objects.bot.setVelocities(0,-.2, 0);
-    }
-
-    if (App.currentState === "LEFT") {
-        if (!App.stateTime) {
-            App.stateTime = Date.now();
-        } else if (Date.now() - App.stateTime > 2200) {
-            App.stateTime = null;
-            App.currentState = "FIND_LINE";
-        }
-
-        App.objects.bot.setVelocityX(0.3);
-    }
-
-    if (App.currentState === "STRAFE_LEFT_TO_BEACON") {
-        if (!App.stateTime) {
-            App.stateTime = Date.now();
-        } else if (currentAngle < 50) { // App.bot.getSide("left") < 10
-            App.stateTime = null;
-            App.currentState = "STRAFE_LEFT_45_TO_BEACON";
-        }
-
-        App.objects.bot.setVelocityX(-0.5);
-    }
-
-    if (App.currentState === "STRAFE_LEFT_45_TO_BEACON") {
-        if (!App.stateTime) {
-            App.stateTime = Date.now();
-        } else if (currentAngle > 85) { // App.bot.getSide("left") < 10
-            App.stateTime = null;
-            App.currentState = null;
-        }
-
-        App.objects.bot.setVelocities(0.4, -0.2, 0.0);
-    }
-
-    if (App.currentState === "STRAFE_TO_BEACON") {
-        if (!App.stateTime) {
-            App.stateTime = Date.now();
-        } else if (xDistToBeacon < 55) { // App.bot.getSide("left") < 10
-            App.stateTime = null;
-            App.currentState = "TELEOP";
-        }
-
-		if (currentAngle > 91) {
-			App.objects.bot.setVelocityY(-0.2);
-		}
-
-		if (currentAngle < 89) {
-			App.objects.bot.setVelocityY(0.2);
-		}
-
-        App.objects.bot.setVelocityX(-0.3);
-    }
-
-	if (App.currentState === "TELEOP") {
-		if (!App.stateTime) {
-            App.stateTime = Date.now();
-        }
-
-		if (App.keysPressed[87]) {
-			App.objects.bot.setVelocityY(0.7);
-		}
-
-		if (App.keysPressed[83]) {
-			App.objects.bot.setVelocityY(-0.7);
-		}
-
-		if (App.keysPressed[65]) {
-			App.objects.bot.setVelocityX(-0.7);
-		}
-
-		if (App.keysPressed[68]) {
-			App.objects.bot.setVelocityX(0.7);
-		}
-
-		if (App.keysPressed[81]) {
-			App.objects.bot.setVelocityR(-75);
-		}
-
-		if (App.keysPressed[69]) {
-			App.objects.bot.setVelocityR(75);
-		}
+	try {
+		document.querySelector('.errors').style.display = 'none';
+		eval(App.code);
+	} catch (e) {
+		console.log(e);
+		document.querySelector('.errors').style.display = 'block';
+		var errorHeading = document.querySelector('.errors > h3');
+		var errorMessage = document.querySelector('.errors > p');
+		errorHeading.textContent = e.name;
+		errorMessage.textContent = e.message;
 	}
 
-    if (App.currentState === "STRAFE_LEFT_BEACON") {
-        if (!App.stateTime) {
-            App.stateTime = Date.now();
-        } else if (Date.now() - App.stateTime > 12000) { // App.bot.getSide("left") < 10
-            App.stateTime = null;
-            App.currentState = null;
-        }
-
-        App.objects.bot.setVelocities(0.1, -0.3, 0.0);
-    }
-
     App.objects.bot.update(interval);
-    if (Date.now() - this.timer > 0) console.log((Date.now() - this.timer) + "ms");
+    if (Date.now() - this.timer > 0) /*console.log((Date.now() - this.timer) + "ms")*/;
 }
 
 function draw() {
